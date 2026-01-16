6f09e03a1d621112cc2f7f7dbeedbbae8230c2e6
Yimmi4000 <yimmi4000@gmail.com>
2026-01-16 01:25:22 -0500

    v0.0.23.4: Modificacion de icono favicon v3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>El Sazon Arepas</title>
-        <link rel="icon" href="/images/favicon.ico.png" type="image/png" />
+        <link rel="icon" href="/images/LOGO.png" type="image/png" />
 
         {/* Metadatos Open Graph */}
         <meta property="og:title" content="El Sazón Arepas" />
