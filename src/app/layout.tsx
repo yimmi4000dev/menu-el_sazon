351db090ab764f4aa48eb5f244ea20e17e7069ff
Yimmi4000 <yimmi4000@gmail.com>
2026-01-16 01:15:01 -0500

    v0.0.23.2: Modificacion de icono favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>El Sazon Arepas - Burguer</title>
+        <title>El Sazon Arepas</title>
 
           {/* Metadatos Open Graph */}
-  <meta property="og:title" content="El Sazón Arepas - Burguer" />
+  <meta property="og:title" content="El Sazón Arepas" />
   <meta property="og:description" content="Explora nuestro menú interactivo y haz tu pedido fácilmente por WhatsApp." />
-  <meta property="og:image" content="https://menu-el-sazon.vercel.app/images/LOGO_Meta.png" />
+  <meta property="og:image" content="https://menu-el-sazon.vercel.app/images/LOGO_Meta_El_Sazon_Arepas.png" />
   <meta property="og:image:width" content="600" />
   <meta property="og:image:height" content="600" />
 
@@ -26,9 +26,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
   {/* Metadatos para Twitter (opcional) */}
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:title" content="El Sazón Arepas - Burguer" />
+  <meta name="twitter:title" content="El Sazón Arepas" />
   <meta name="twitter:description" content="Explora nuestro menú interactivo y haz tu pedido fácilmente por WhatsApp." />
-  <meta name="twitter:image" content="https://menu-el-sazon.vercel.app/images/LOGO_Meta.png" />
+  <meta name="twitter:image" content="https://menu-el-sazon.vercel.app/images/LOGO_Meta_El_Sazon_Arepas.png" />
 
       </head>
       <body className="bg-gray-100 min-h-screen flex flex-col overflow-y-scroll">
